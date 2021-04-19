ca6957e29c89ad160897e20a0339b5d5185a96fd
Marwan Abdelatti <engineer.marwan@gmail.com>
2021-04-19 11:29:21 -0400

    added launch and remote environments config files

---
+++ src/hbw/env_hbw.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export ROS_MASTER_URI=http://192.168.0.10:11311
+export ROS_HOSTNAME=192.168.0.12
+
+source /opt/ros/melodic/setup.bash
+source /home/ubuntu/catkin_ws/devel/setup.bash
+
+exec "$@"
