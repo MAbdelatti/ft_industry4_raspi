bae2bc4358249fb4648d10373c906df34ac28a8c
MarwanAbdelatti <engineer.marwan@gmail.com>
2021-05-19 10:05:24 -0400

    Added multi-location for the both in and out processing

--- src/hbw/hbw_ROS_enabled.ino
+++ src/hbw/hbw_ROS_enabled.ino
@@ -12,6 +12,10 @@ std_msgs::String str_msg;
 ros::Publisher update_empty("what_is_empty", &str_msg);
 char empty_payload[3] = "D4";
 
+std_msgs::String str_msg_occupied;
+ros::Publisher update_occupied("what_is_occupied", &str_msg_occupied);
+char occupied_payload[9] = "D4_blue";
+
 void bring_back_home(){
   //Vertical motor setup
   uint16_t button2 = ftduino.input_get(Ftduino::I8);
@@ -40,8 +44,14 @@ void bring_back_home(){
   ftduino.motor_set(Ftduino::M4, Ftduino::OFF, Ftduino::MAX);
 }
 
-void move_robot_outprocessing(uint16_t x, uint16_t y){
-  nh.loginfo("HBW Received an Order");
+void move_robot_to_shelf(uint16_t x, uint16_t y, String _caller){
+  if (_caller == "out_processing"){
+    nh.loginfo("HBW Received an Order");
+  }
+  else if (_caller == "in_processing"){
+    nh.loginfo("HBW is Preparing an Empty Box");
+  }
+  
   if(ftduino.input_get(Ftduino::I5)){
    ftduino.motor_counter_set_brake
    (Ftduino::M2, false);
@@ -67,7 +77,13 @@ void move_robot_outprocessing(uint16_t x, uint16_t y){
     ftduino.motor_counter_set_brake(Ftduino::M4, false);
     ftduino.motor_counter(Ftduino::M4, Ftduino::RIGHT, Ftduino::MAX, 100);
     while(ftduino.motor_counter_active(Ftduino::M4));
-    nh.loginfo("HBW Picked Up Material");
+    
+    if (_caller == "out_processing"){
+      nh.loginfo("HBW Picked Up Material");
+    }
+    else if (_caller == "in_processing"){
+      nh.loginfo("HBW Picked Empty Box from The Shelf");
+    }
   
     // Retract robot arm
     uint16_t button1 = ftduino.input_get(Ftduino::I6);
@@ -103,7 +119,13 @@ void move_robot_outprocessing(uint16_t x, uint16_t y){
    // Lower the robot's arm, wait for a period of three seconds, then raise the arm again 
    ftduino.motor_counter(Ftduino::M4, Ftduino::LEFT, Ftduino::MAX, 90);
    while(ftduino.motor_counter_active(Ftduino::M4));
-   nh.loginfo("HBW Placed Material on Belt");
+   
+   if (_caller == "out_processing"){
+     nh.loginfo("HBW Placed Material on Belt");
+   }
+   else if(_caller == "in_processing"){
+     nh.loginfo("HBW Placed Box on the Belt");
+   }
    
    if(!ftduino.input_get(Ftduino::I4)){
       ftduino.motor_set(Ftduino::M1, Ftduino::LEFT, Ftduino::MAX);
@@ -112,18 +134,35 @@ void move_robot_outprocessing(uint16_t x, uint16_t y){
 
       delay(5000);
       
-      nh.loginfo("HBW Material is Picked by VGR");
+      if (_caller == "out_processing"){
+        nh.loginfo("HBW Material is Picked by VGR");
+      }
+      else if (_caller == "in_processing"){
+        nh.loginfo("HBW Material is Dropped by VGR");
+      }
       
       ftduino.motor_set(Ftduino::M1, Ftduino::RIGHT, Ftduino::MAX);
       delay(1000);
       ftduino.motor_set(Ftduino::M1, Ftduino::OFF, Ftduino::MAX);
       delay(1000);
-      nh.loginfo("HBW Empty Box is Ready for Pick Up");
+      
+      if (_caller == "out_processing"){
+        nh.loginfo("Empty Box is Ready for HBW Pick Up");
+      }
+      else if (_caller == "in_processing"){
+        nh.loginfo("Material is Ready for HBW Pick Up");
+      }
   }}
    
    ftduino.motor_counter(Ftduino::M4, Ftduino::RIGHT, Ftduino::MAX, 90);
    while(ftduino.motor_counter_active(Ftduino::M4));
-   nh.loginfo("HBW Picked Up the Empty Box");
+
+   if (_caller == "out_processing"){
+     nh.loginfo("HBW Picked up the Empty Box from the Belt");
+   }
+   else if (_caller == "in_processing"){
+     nh.loginfo("HBW Picked up Material from the Belt");
+   }
   
    // Retract the robot's arm fully
    uint16_t button3 = ftduino.input_get(Ftduino::I6);
@@ -139,8 +178,7 @@ void move_robot_outprocessing(uint16_t x, uint16_t y){
     else{
       ftduino.motor_counter(Ftduino::M4, Ftduino::LEFT, Ftduino::MAX, y-790);
     }
-    while(ftduino.motor_counter_active(Ftduino::M4));
-    
+    while(ftduino.motor_counter_active(Ftduino::M4));    
   
    // Return the robot to the inventory rack position 
     ftduino.motor_counter(Ftduino::M2, Ftduino::LEFT, Ftduino::MAX, x-40);
@@ -155,16 +193,30 @@ void move_robot_outprocessing(uint16_t x, uint16_t y){
     }
     //turns off motor
     ftduino.motor_set(Ftduino::M3, Ftduino::OFF, Ftduino::MAX);
+    
     nh.loginfo("HBW is Set to Home");
   
    // Lower the robot's arm, wait for a period of three seconds 
    ftduino.motor_counter(Ftduino::M4, Ftduino::LEFT, Ftduino::MAX, 100);
    while(ftduino.motor_counter_active(Ftduino::M4));
-   nh.loginfo("HBW Empty Box is Back on the Shelf");
+   
+   if (_caller == "out_processing"){
+     nh.loginfo("HBW Empty Box is Back on the Shelf");
+   }
+   else if (_caller == "in_processing"){
+     nh.loginfo("HBW Placed Material on the Shelf");
+   }
 
-   //publish the shelf name to the topic what_is_empty:
-   str_msg.data = empty_payload;
-   update_empty.publish( &str_msg );
+   if (_caller == "out_processing"){
+     //publish the shelf name to the topic what_is_empty:
+     str_msg.data = empty_payload;
+     update_empty.publish( &str_msg );
+   }
+   else if (_caller == "in_processing"){
+     //publish the shelf name to the topic what_is_occupied:
+     str_msg_occupied.data = occupied_payload;
+     update_occupied.publish( &str_msg_occupied );
+   }   
    nh.spinOnce();
      
    // Return to home position
@@ -172,206 +224,152 @@ void move_robot_outprocessing(uint16_t x, uint16_t y){
    nh.loginfo("HBW Robot is Set to Home");
   }
 
-
 void out_processing(const std_msgs::String &payload_msg){
 
   strcpy(empty_payload, payload_msg.data);
+  uint16_t x;
+  uint16_t y;
+
+  String shelf_loc = String(payload_msg.data);
   
-  if(String(payload_msg.data)=="A1" && move_hbw){
+  if(shelf_loc == "A1" && move_hbw){
     move_hbw = false;
-    uint16_t x = 810;
-    uint16_t y = 100;
-    move_robot_outprocessing(x, y); 
+    x = 810;
+    y = 150; 
+    move_robot_to_shelf(x, y, "out_processing");
  }
 
-  else if(String(payload_msg.data)=="A2" && move_hbw){
+  else if(shelf_loc == "A2" && move_hbw){
     move_hbw = false;
-    uint16_t x = 1405;
-    uint16_t y = 100;
-    move_robot_outprocessing(x, y); 
+    x = 1405;
+    y = 150;
+    move_robot_to_shelf(x, y, "out_processing");
  }
 
-  else if(String(payload_msg.data)=="A3" && move_hbw){
+  else if(shelf_loc == "A3" && move_hbw){
     move_hbw = false;
-    uint16_t x = 2020;
-    uint16_t y = 100;
-    move_robot_outprocessing(x, y); 
+    x = 2020;
+    y = 150;
+    move_robot_to_shelf(x, y, "out_processing");
  }
 
-  else if(String(payload_msg.data)=="B1" && move_hbw){
+  else if(shelf_loc == "B1" && move_hbw){
     move_hbw = false;
-    uint16_t x = 810;
-    uint16_t y = 450;
-    move_robot_outprocessing(x, y); 
+    x = 810;
+    y = 450;
+    move_robot_to_shelf(x, y, "out_processing");
  }
 
-  else if(String(payload_msg.data)=="B2" && move_hbw){
+  else if(shelf_loc == "B2" && move_hbw){
     move_hbw = false;
-    uint16_t x = 1405;
-    uint16_t y = 450;
-    move_robot_outprocessing(x, y); 
+    x = 1405;
+    y = 450;
+    move_robot_to_shelf(x, y, "out_processing");
  }
 
-  else if(String(payload_msg.data)=="B3" && move_hbw){
+  else if(shelf_loc == "B3" && move_hbw){
     move_hbw = false;
-    uint16_t x = 2020;
-    uint16_t y = 450;
-    move_robot_outprocessing(x, y); 
+    x = 2020;
+    y = 450;
+    move_robot_to_shelf(x, y, "out_processing");
  }
 
-  else if(String(payload_msg.data)=="C1" && move_hbw){
+  else if(shelf_loc == "C1" && move_hbw){
     move_hbw = false;
-    uint16_t x = 810;
-    uint16_t y = 840;
-    move_robot_outprocessing(x, y); 
+    x = 810;
+    y = 840;
+    move_robot_to_shelf(x, y, "out_processing");
  }
 
-  else if(String(payload_msg.data)=="C2" && move_hbw){
+  else if(shelf_loc == "C2" && move_hbw){
     move_hbw = false;
-    uint16_t x = 1405;
-    uint16_t y = 840;
-    move_robot_outprocessing(x, y); 
+    x = 1405;
+    y = 840;
+    move_robot_to_shelf(x, y, "out_processing");
  }
 
-  else if(String(payload_msg.data)=="C3" && move_hbw){
+  else if(shelf_loc == "C3" && move_hbw){
     move_hbw = false;
-    uint16_t x = 2020;
-    uint16_t y = 840;
-    move_robot_outprocessing(x, y);
+    x = 2020;
+    y = 840;
+    move_robot_to_shelf(x, y, "out_processing");
  }
+ 
  move_hbw = true;
 }
 
 void in_processing(const std_msgs::String &payload_msg){
-  if (payload_msg.data){
-    
-   nh.loginfo("HBW is Preparing an Empty Box"); 
-   //Move robot from home position to first inventory rack position
+
+  strcpy(occupied_payload, payload_msg.data);
+  uint16_t x;
+  uint16_t y;
+
+  String shelf_loc = String(payload_msg.data).substring(0,2);
    
-   if(ftduino.input_get(Ftduino::I5)){
-   ftduino.motor_counter_set_brake(Ftduino::M2, false);
-   ftduino.motor_counter(Ftduino::M2, Ftduino::LEFT, Ftduino::MAX, 810);
-   while(ftduino.motor_counter_active(Ftduino::M2));
-   ftduino.motor_counter_set_brake(Ftduino::M4, false);
-   ftduino.motor_counter(Ftduino::M4, Ftduino::LEFT, Ftduino::MAX, 100);
-   while(ftduino.motor_counter_active(Ftduino::M4));
-   
-   // Extend robot arm 
-    uint16_t button6 = ftduino.input_get(Ftduino::I7);
-    //starts motor
-    ftduino.motor_set(Ftduino::M3, Ftduino::LEFT, Ftduino::MAX);
-    while(!button6){
-      button6 = ftduino.input_get(Ftduino::I7);
-    }
-    //turns off motor
-    ftduino.motor_set(Ftduino::M3, Ftduino::OFF, Ftduino::MAX);
-  
-    // Raise robot arm
-    
-    ftduino.motor_counter_set_brake(Ftduino::M4, false);
-    ftduino.motor_counter(Ftduino::M4, Ftduino::RIGHT, Ftduino::MAX, 100);
-    while(ftduino.motor_counter_active(Ftduino::M4));
-    nh.loginfo("HBW Picked a Box from The Shelf");
-  
-    // Retract robot arm
-    uint16_t button7 = ftduino.input_get(Ftduino::I6);
-    //turns motor back on, in other direction
-    ftduino.motor_set(Ftduino::M3, Ftduino::RIGHT, Ftduino::MAX);
-    while(!button7){
-      button7 = ftduino.input_get(Ftduino::I6);
-     }
-    ftduino.motor_set(Ftduino::M3, Ftduino::OFF, Ftduino::MAX);
-  
-    //Return robot to conveyor unit
-  
-   ftduino.motor_counter_set_brake(Ftduino::M4, false);
-   ftduino.motor_counter(Ftduino::M2, Ftduino::RIGHT, Ftduino::MAX, 768);
-   while(ftduino.motor_counter_active(Ftduino::M2));
-   ftduino.motor_counter(Ftduino::M4, Ftduino::LEFT, Ftduino::MAX, 650);
-   while(ftduino.motor_counter_active(Ftduino::M4));
-  
-   // Extend the robot's arm fully
-   uint16_t button8 = ftduino.input_get(Ftduino::I7);
-    //starts motor
-    ftduino.motor_set(Ftduino::M3, Ftduino::LEFT, Ftduino::MAX);
-    while(!button8){
-      button8 = ftduino.input_get(Ftduino::I7);
-    }
-    //turns off motor
-    ftduino.motor_set(Ftduino::M3, Ftduino::OFF, Ftduino::MAX);
-    nh.loginfo("HBW Placed Box on the Belt");
-  
-   // Lower the robot's arm, wait for a period of three seconds, then raise the arm again 
-   ftduino.motor_counter(Ftduino::M4, Ftduino::LEFT, Ftduino::MAX, 90);
-   while(ftduino.motor_counter_active(Ftduino::M4));
-   
-   if(!ftduino.input_get(Ftduino::I4)){
-      ftduino.motor_set(Ftduino::M1, Ftduino::LEFT, Ftduino::MAX);
-      delay(1000);
-      ftduino.motor_set(Ftduino::M1, Ftduino::OFF, Ftduino::MAX);
-      delay(5000);
-      ftduino.motor_set(Ftduino::M1, Ftduino::RIGHT, Ftduino::MAX);
-      delay(1000);
-      ftduino.motor_set(Ftduino::M1, Ftduino::OFF, Ftduino::MAX);
-      delay(1000);
-  
-  }}
-   
-   ftduino.motor_counter(Ftduino::M4, Ftduino::RIGHT, Ftduino::MAX, 90);
-   while(ftduino.motor_counter_active(Ftduino::M4));
-  
-   // Retract the robot's arm fully
-   uint16_t button9 = ftduino.input_get(Ftduino::I6);
-    //turns motor back on, in other direction
-    ftduino.motor_set(Ftduino::M3, Ftduino::RIGHT, Ftduino::MAX);
-    while(!button9){
-      button9 = ftduino.input_get(Ftduino::I6);
-     }
-    ftduino.motor_set(Ftduino::M3, Ftduino::OFF, Ftduino::MAX);
-    
-    ftduino.motor_counter(Ftduino::M4, Ftduino::RIGHT, Ftduino::MAX, 650);
-    while(ftduino.motor_counter_active(Ftduino::M4));
-    nh.loginfo("HBW Picked Material from the Belt");
-    
-  
-   // Return the robot to the inventory rack position 
-    ftduino.motor_counter(Ftduino::M2, Ftduino::LEFT, Ftduino::MAX, 768);
-    while(ftduino.motor_counter_active(Ftduino::M2));
-   
-    // Extend robot arm 
-    uint16_t button10 = ftduino.input_get(Ftduino::I7);
-    //starts motor
-    ftduino.motor_set(Ftduino::M3, Ftduino::LEFT, Ftduino::MAX);
-    while(!button10){
-      button10 = ftduino.input_get(Ftduino::I7);
-    }
-    //turns off motor
-    ftduino.motor_set(Ftduino::M3, Ftduino::OFF, Ftduino::MAX);
-  
-   // Lower the robot's arm, wait for a period of three seconds 
-   ftduino.motor_counter(Ftduino::M4, Ftduino::LEFT, Ftduino::MAX, 100);
-   while(ftduino.motor_counter_active(Ftduino::M4));
-   nh.loginfo("HBW Placed Material on the Shelf");
-  
-   // Retract the robot's arm fully
-   uint16_t button11 = ftduino.input_get(Ftduino::I6);
-    //turns motor back on, in other direction
-    ftduino.motor_set(Ftduino::M3, Ftduino::RIGHT, Ftduino::MAX);
-    while(!button11){
-      button11 = ftduino.input_get(Ftduino::I6);
-     }
-    ftduino.motor_set(Ftduino::M3, Ftduino::OFF, Ftduino::MAX);
-  
-   // Return to home position
-   
-   ftduino.motor_counter(Ftduino::M4, Ftduino::RIGHT, Ftduino::MAX, 100);
-   while(ftduino.motor_counter_active(Ftduino::M4));
-   ftduino.motor_counter(Ftduino::M2, Ftduino::RIGHT, Ftduino::MAX, 810);
-   while(ftduino.motor_counter_active(Ftduino::M2));
-   nh.loginfo("HBW Robot is Set to Home");
-  
-   // END OF IN-PROCESSING
-  }
+  if(shelf_loc == "A1" && move_hbw){
+    move_hbw = false;
+    x = 810;
+    y = 150; 
+    move_robot_to_shelf(x, y, "in_processing");
+ }
+
+  else if(shelf_loc == "A2" && move_hbw){
+    move_hbw = false;
+    x = 1405;
+    y = 150;
+    move_robot_to_shelf(x, y, "in_processing");
+ }
+
+  else if(shelf_loc == "A3" && move_hbw){
+    move_hbw = false;
+    x = 2020;
+    y = 150;
+    move_robot_to_shelf(x, y, "in_processing");
+ }
+
+  else if(shelf_loc == "B1" && move_hbw){
+    move_hbw = false;
+    x = 810;
+    y = 450;
+    move_robot_to_shelf(x, y, "in_processing");
+ }
+
+  else if(shelf_loc == "B2" && move_hbw){
+    move_hbw = false;
+    x = 1405;
+    y = 450;
+    move_robot_to_shelf(x, y, "in_processing");
+ }
+
+  else if(shelf_loc == "B3" && move_hbw){
+    move_hbw = false;
+    x = 2020;
+    y = 450;
+    move_robot_to_shelf(x, y, "in_processing");
+ }
+
+  else if(shelf_loc == "C1" && move_hbw){
+    move_hbw = false;
+    x = 810;
+    y = 840;
+    move_robot_to_shelf(x, y, "in_processing");
+ }
+
+  else if(shelf_loc == "C2" && move_hbw){
+    move_hbw = false;
+    x = 1405;
+    y = 840;
+    move_robot_to_shelf(x, y, "in_processing");
+ }
+
+  else if(shelf_loc == "C3" && move_hbw){
+    move_hbw = false;
+    x = 2020;
+    y = 840;
+    move_robot_to_shelf(x, y, "in_processing");
+ }
+ 
+ move_hbw = true;
 }
 
 ros::Subscriber<std_msgs::String> new_order_sub("new_order", &out_processing);
@@ -383,6 +381,7 @@ void setup() {
   nh.subscribe(new_order_sub);
   nh.subscribe(vgr_new_material_pub);
   nh.advertise(update_empty);
+  nh.advertise(update_occupied);
   ftduino.init();
 }
 
@@ -390,8 +389,6 @@ void loop() {
   // put your main code here, to run repeatedly:
  
  nh.spinOnce(); // IN-PROCESSING
-
-// out_processing();/
 
  delay(250);
 }
